5b84c75c5566639319474e5766e6104a5515d227
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-08-03 12:00:34 +0600

    added task4

---
+++ array4.js
@@ -0,0 +1,29 @@
+let arr1=[1,2,3,4,5];
+let arr2="Hello, world!";
+let arr3=40;
+let arr4=["apple","banana"];
+
+if(Array.isArray(arr1)){
+    console.log("Array1=Yes");
+}
+else{
+    console.log("Array1=No");
+}
+if(Array.isArray(arr2)){
+    console.log("Array2=Yes");
+}
+else{
+    console.log("Array2=No");
+}
+if(Array.isArray(arr3)){
+    console.log("Array3=Yes");
+}
+else{
+    console.log("Array3=No");
+}
+if(Array.isArray(arr4)){
+    console.log("Array4=Yes");
+}
+else{
+    console.log("Array4=No");
+}
