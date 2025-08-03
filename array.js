fc4966cf29c8a0bed6f3a4daa908b5d24595f4be
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-08-03 11:31:56 +0600

    added array task 2

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
 let fruits=["Mango","Lemon","apple", "banana", "orange"];
- fruits[1]=["Jambura"];
+ fruits[1]="Jambura";
 console.log(fruits);
 console.log(fruits[2]);
---
+++ array2.js
@@ -0,0 +1,7 @@
+let touristDestination=["Kuakata","Bandarban","Cox's-Bazar"];
+touristDestination.push("Rangamati")
+touristDestination.push("Lalbag Kella")
+touristDestination.push("Shalbon Bihar")
+console.log(touristDestination.join(","));
+touristDestination.pop();
+console.log(touristDestination);
