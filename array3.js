69bf30ebf067580118e7618d4da3bf616fb3c7d9
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-08-03 11:47:14 +0600

    added task 3

---
+++ array3.js
@@ -0,0 +1,11 @@
+let book=["Physics","Chemisry","Mathmetics"];
+console.log(book);
+console.log(book.includes("Mathmetics"));
+
+let search=book.includes("Mathmetics");
+if(search==book.includes("Mathmetics"){
+    console.log("Physics is in here");
+}
+else{
+    console.log("Physics is not in here");
+}
