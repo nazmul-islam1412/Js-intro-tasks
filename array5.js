1da6962491cfdc1eefc978db62d8b5370ac5e7b2
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-08-03 12:10:04 +0600

    added task5

---
+++ array5.js
@@ -0,0 +1,8 @@
+let arr1=["Mango","Lemon","apple", "banana", "orange"];
+let arr2=["Kuakata","Bandarban","Cox's-Bazar"];
+
+console.log(arr1);
+console.log(arr2);
+
+let combinedArray=arr1.concat(arr2);
+console.log(combinedArray);
